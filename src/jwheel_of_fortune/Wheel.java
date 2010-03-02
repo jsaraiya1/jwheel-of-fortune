cb2b426aee032a37501e36238b631c234f345e68
jpgunter <jpgunter@d5cc4b60-22f4-11df-a154-f73f31f273f6>
2010-03-02 15:34:22 +0000

    Re-arrange source packages

---
+++ src/jwheel_of_fortune/Wheel.java
@@ -0,0 +1,28 @@
+package jwheel_of_fortune;
+
+/**
+ * @author James Gunter
+ * Wheel.java
+ * This class 
+ */
+public class Wheel {
+    //static variables to map to values in the values array;
+    public static final int BANKRUPT = 0;
+    public static final int LOSE_A_TURN = -1;
+    private int[] values;
+    private int currentPosition;
+
+    public Wheel()
+    {
+        values = new int[50];
+        currentPosition = 0;
+    }
+
+    public int spin()
+    {
+        int landedOn = 1;
+        currentPosition = 1;
+        return values[1];
+    }
+
+}
