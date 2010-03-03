cd694a3bab58bfa64bb398dd87815f149d437ef4
jpgunter <jpgunter@d5cc4b60-22f4-11df-a154-f73f31f273f6>
2010-03-03 00:53:44 +0000

    Updated Main a little, and tweaked the Player Class, there really isn't much there.

--- src/jwheel_of_fortune/Main.java
+++ src/jwheel_of_fortune/Main.java
@@ -34,29 +34,38 @@ public class Main
         {
             Board b = new Board(new File("dictionary.txt"));
             Wheel w = new Wheel(Wheel.DOUBLE_TYPE);
-            int curSpin = w.spin();
-            char guess = ' ';
 
-            for(int i=0;i<50;i++)
-                System.out.println(w.spin());
+            System.out.print("--Wheel of Fortune--\n\n" +
+                    "Enter your name: ");
+            is.useDelimiter("\n");
+            Player p1 = new Player(is.next(),null);
+            char guess = ' ';
+            int curSpin = w.spin();
 
             do
             {
                 System.out.println("Type: " + b.getCategory());
                 System.out.println(b.getCurrentPhrase());
+                System.out.println("Your current money total: $" + p1.getCurrentScore());
                 curSpin = w.spin();
                 switch(curSpin){
                     case Wheel.BANKRUPT:
-                        //they get nothing and lose money.
+                        System.out.println("Sorry you landed on Bankrupt.");
+                        p1.addPoints(p1.getCurrentScore() * -1);
                         break;
                     case Wheel.LOSE_A_TURN:
-                        //nothin but keep money
                         break;
                     default:
                         System.out.print("You landed on $"+ curSpin +"\nEnter a guess: ");
                         guess = is.next().charAt(0);
                         int letters = b.guessLetter(guess);
+                        while(letters < 0)
+                        {
+                            System.out.println("You already guessed that letter.\nEnter another guess: ");
+                            guess = is.next().charAt(0);
+                        }
                         int winnings = letters * curSpin;
+                        p1.addPoints(winnings);
                         System.out.printf("There were %d %s's in that phrase. You won: %d\n", letters, guess, winnings);
                         if (!b.getCurrentPhrase().contains("_"))
                         {
